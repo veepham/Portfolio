e53471477211c94d5e4ed39315aba17746a8d513
veepham <111474183+veepham@users.noreply.github.com>
2023-03-23 18:19:14 -0700

    Rebuilt portfolio website
    Rebuilt portfolio website using ASP.NET Core Web App template

---
+++ Portfolio/Portfolio/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+document.addEventListener("click", function (event) {
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")) {
+        closeForm()
+    }
+}, false)
